d024a9bf1f7b90f889593768a7685cc90a534577
jack <jack900707@gmail.com>
2022-07-02 00:33:03 +0800

    add color.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+from color import*
 import pygame
+from spot import spot
+WIDTH = 800
+PAD = 50
+NUMBER = 20
+DIAMETER = (WIDTH-2*PAD)/(2*NUMBER-1)
+RADIUS = DIAMETER/2
+GRID=[[spot(RADIUS,PAD+RADIUS+2*i*DIAMETER,PAD+RADIUS+2*j*DIAMETER,i,j) for j in range(NUMBER)] for i in range(NUMBER)]
+def draw_begin(win):
+    for i in range(NUMBER):
+        for j in range(NUMBER):
+            GRID[i][j].show(win)
 if __name__ == '__main__':
-    WIDTH = 800
+    
+    pygame.init()
     win = pygame.display.set_mode((WIDTH,WIDTH))
     pygame.display.set_caption('Path finding visualization')
+    win.fill(WHITE)
+    draw_begin(win)
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        pygame.display.update()
     pygame.quit()
--- spot.py
+++ spot.py
@@ -1,6 +1,5 @@
-WHITE = (0,0,0)
-BLACK = (255,255,255)
-TEAL = (104,250,240)
+from color import*
+import pygame
 class spot:
     def __init__(self,r,x,y,row,col):
         self.set_radius(r)
@@ -8,21 +7,51 @@ class spot:
         self.set_color(WHITE)
         self.set_pos((row,col))
     def set_color(self,c):
-        self.color = c
+        self._color = c
     def set_pos(self,p):
-        self.pos = p
+        self._pos = p
     def set_radius(self,r):
-        self.radius = r
+        self._radius = r
     def set_coord(self,c):
-        self.coord = c    
+        self._coord = c    
     def get_coord(self):
-        return self.x,self.y
+        return self._coord
     def get_color(self):
-        return self.color
+        return self._color
     def get_pos(self):
-        return self.row, self.col
+        return self._pos
     def get_radius(self):
-        return self.radius
+        return self._radius
+    def make_begin(self):
+        self.set_color(GREEN)
+    def make_end(self):
+        self.set_color(RED)
+    def make_block(self):
+        self.set_color(BLACK)
+    def make_origin(self):
+        self.set_color(WHITE)
+    def make_done(self):
+        self.set_color(TEAL)
+    def make_current(self):
+        self.set_color(PURPLE)
+    def make_path(self):
+        self.set_color(YELLOW)
+    def is_block(self):
+        return self.color == BLACK
+    def is_begin(self):
+        return self.color == WHITE
+    def is_end(self):
+        return self.color == RED
+    def is_origin(self):
+        return self.color == GREEN
+    def is_done(self):
+        return self.color == TEAL
+    def is_current(self):
+        return self.color == PURPLE
+    def show(self,win):
+        pygame.draw.circle(win,self._color,self._coord,self._radius,0)
+        pygame.draw.circle(win,BLACK,self._coord,self._radius,3)
+        pygame.display.update()
     coord = property(lambda self:self.get_coord,
                      lambda self,c:self.set_coord(c))
     color = property(lambda self:self.get_color,
